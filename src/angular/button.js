80512efb370ba978fd62ba164247e899620b8c53
Kostas Tsalikidis <tsalikidhs@gmail.com>
2016-08-13 21:39:16 +0300

    button components

---
+++ src/angular/button.js
@@ -0,0 +1,53 @@
+angular
+  .module('lux')
+  .directive('lButton', function() {
+    return {
+      restrict: 'EA',
+      link: function(scope, element, attrs) {
+        element.addClass('button');
+        
+        attrs.$observe('type', function(val) {
+          var validTypes = ['white', 'light', 'dark', 'black', 'link', 'primary', 'info', 'success', 'warning', 'danger'];
+          element.removeClass('is-' + validTypes.join(' is-'));
+          if (validTypes.indexOf(val) > -1) {
+            element.addClass('is-' + val);
+          }
+        });
+        
+        attrs.$observe('size', function(val) {
+          var validSizes = ['small', 'medium', 'large'];
+          element.removeClass('is-' + validSizes.join(' is-'));
+          if (validSizes.indexOf(val) > -1) {
+            element.addClass('is-' + val);  
+          }
+        });
+        
+        ['outlined', 'inverted', 'loading', 'active', 'disabled'].forEach(function(styleState) {
+          attrs.$observe(styleState, function(val) {
+            if (val == 'false') { // because boolean values end up as string values
+              element.removeClass('is-' + styleState);
+            } else {
+              element.addClass('is-' + styleState);
+            }
+          });
+        });
+      }
+    };
+  })
+  .directive('addonsButton', function() {
+    return {
+      restrict: 'A',
+      scope: {
+        selected: '=addonsButton'
+      },
+      link: function(scope, element, attrs) {
+        scope.selected = 'ddd';
+        angular.forEach(element.children(), function(button) {
+          angular.element(button).on('click', function() {
+            scope.selected = this.dataset.value;
+            setTimeout(function() { scope.$apply(); }, 0); // force diggest
+          });
+        });
+      }
+    };
+  })
---
+++ src/vue/button.js
@@ -0,0 +1,39 @@
+Lux.ButtonComponent = {
+  replace: true,
+  template: '<button class="button" ' +
+    'v-bind:class="[ ' +
+    'type !== undefined ? \'is-\' + type : \'\', ' +
+    'size !== undefined ? \'is-\' + size : \'\', ' +
+    'outlined == true ? \'is-outlined\' : \'\', ' +
+    'inverted == true ? \'is-inverted\' : \'\', ' +
+    'loading == true ? \'is-loading\' : \'\', ' +
+    'active == true ? \'is-active\' : \'\', ' +
+    'disabled == true ? \'is-disabled\' : \'\', ' +
+    ']">{{{text || content}}}</button>',
+  props: [
+    'text',
+    'content',
+    'type',
+    'size',
+    'outlined',
+    'inverted',
+    'loading',
+    'active',
+    'disabled'
+  ]
+};
+
+Lux.AddonsButton = {
+  twoWay: true,
+  bind: function() {
+    var vm = this;
+    for (var i = 0; i < vm.el.children.length; i++) {
+      vm.el.children[i].addEventListener('click', function() {
+        var previousActive = vm.el.querySelector('.is-active');
+        if (previousActive) previousActive.setAttribute('class', previousActive.getAttribute('class').replace('is-active', ''));
+        vm.set(this.dataset.value);
+        this.setAttribute('class', this.getAttribute('class') + ' is-active');
+      });
+    }
+  }
+};
